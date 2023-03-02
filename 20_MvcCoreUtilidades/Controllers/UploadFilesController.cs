cdd59e0d0e29168697a3f26c623815e339093665
NERFOO <james.jaime.1105@gmail.com>
2023-03-02 14:25:38 +0100

    Add project files.

---
+++ 20_MvcCoreUtilidades/Controllers/UploadFilesController.cs
@@ -0,0 +1,39 @@
+﻿using _20_MvcCoreUtilidades.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _20_MvcCoreUtilidades.Controllers
+{
+    public class UploadFilesController : Controller
+    {
+
+        private HelperPathProvider helperPath;
+
+        public UploadFilesController(HelperPathProvider helperPath)
+        {
+            this.helperPath = helperPath;
+        }
+
+        public IActionResult SubirFichero()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SubirFichero(IFormFile fichero)
+        {
+            string fileName = fichero.FileName;
+
+            string path = this.helperPath.MapPath(fileName, Folders.Uploads);
+
+            using (Stream stream = new FileStream(path, FileMode.Create))
+            {
+                await fichero.CopyToAsync(stream);
+            }
+
+            ViewData["MENSAJE"] = "Fichero subido a " + path;
+            ViewData["URL"] = "<a href=''>Mi fichero</a>";
+
+            return View();
+        }
+    }
+}
